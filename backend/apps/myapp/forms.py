169f2f5dbff6054ae3fdba3539554efaef2cf238
dmytro <rungotika@gmail.com>
2023-09-27 17:54:39 +0200

    SMTP connection added

--- backend/apps/myapp/forms.py
+++ backend/apps/myapp/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.core.validators import validate_email
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(required=True, max_length=255,
-                             widget=forms.TextInput())
-    subject = forms.CharField(required=True, max_length=255)
-    message = forms.Textarea(deffault_attrs)
-
+    email = forms.EmailField(required=True, max_length=255, validators=[validate_email],
+                             widget=forms.TextInput({"type": "email"}))
+    subject = forms.CharField(required=True, max_length=255,
+                              widget=forms.TextInput({"type": "text"}))
+    message = forms.CharField(required=True, max_length=10 ** 4,
+                              widget=forms.Textarea(
+                                  attrs={"cols": 30, "rows": 10}))
--- backend/apps/myapp/views.py
+++ backend/apps/myapp/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import FormView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.views import View
+from django.conf import settings
 
 from .forms import ContactForm
 
 
-class ContactView(FormView):
+class ContactView(View):
     form_class = ContactForm
     template_name = "apps.myapp/contact.html"
 
@@ -13,4 +16,21 @@ class ContactView(FormView):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        pass
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            messages.success(request,
+                             message='Email has been sent successfully!')
+            send_mail(
+                subject="Contact me",
+                message=f"From: {request.POST['email']}\n\nSubject: {request.POST['subject']}\n\n" +
+                        f"Message: {request.POST['message']}\n",
+                from_email=settings.FROM_EMAIL,
+                recipient_list=[settings.RECIPIENT_EMAIL],
+                fail_silently=False
+            )
+            return redirect("contact")
+
+        messages.error(request, message="Ops... Check the data you entered and try again!")
+
+        return render(request, self.template_name, {'form': form})
