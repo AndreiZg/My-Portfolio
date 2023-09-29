0dd114a43c60bc3b0b21b875c0f14c8384c2a939
dmytro <rungotika@gmail.com>
2023-09-29 18:52:10 +0200

    projects page added

--- backend/apps/myapp/urls.py
+++ backend/apps/myapp/urls.py
@@ -6,5 +6,7 @@ from .views import ContactView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="apps.myapp/welcome.html"), name='welcome'),
     path('about/', TemplateView.as_view(template_name="apps.myapp/about.html"), name='about'),
+    path('projects/', TemplateView.as_view(template_name="apps.myapp/projects.html"), name='projects'),
     path('contact/', ContactView.as_view(), name='contact')
+
 ]
